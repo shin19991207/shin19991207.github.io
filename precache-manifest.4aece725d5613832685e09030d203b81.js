c18941bcaf73b7ca8d8a55bde1329f26853e9ae1
shin19991207@gmail.com <shin19991207@gmail.com>
2022-01-12 03:16:17 -0500

    Updates

---
+++ precache-manifest.4aece725d5613832685e09030d203b81.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "407da7141fcaa8cb621c019afb403fe6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50379aae59333fa2a0c5",
+    "url": "/static/css/2.5bc09813.chunk.css"
+  },
+  {
+    "revision": "5543eedc3dc38a3d907e",
+    "url": "/static/css/main.84df45ac.chunk.css"
+  },
+  {
+    "revision": "50379aae59333fa2a0c5",
+    "url": "/static/js/2.eaaa40fe.chunk.js"
+  },
+  {
+    "revision": "b5c8281ca89c1ddb89c15ead0e00f32f",
+    "url": "/static/js/2.eaaa40fe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5543eedc3dc38a3d907e",
+    "url": "/static/js/main.d7032f30.chunk.js"
+  },
+  {
+    "revision": "e1a4e00d23091b34c741",
+    "url": "/static/js/runtime-main.6bc1d974.js"
+  },
+  {
+    "revision": "9717a508cee2991d8e3a8f4f3db0e949",
+    "url": "/static/media/profile.9717a508.JPG"
+  },
+  {
+    "revision": "31cfbb24923a55d3bfae9cb7789d1f31",
+    "url": "/static/media/sidebar_bg.31cfbb24.jpg"
+  }
+]);
